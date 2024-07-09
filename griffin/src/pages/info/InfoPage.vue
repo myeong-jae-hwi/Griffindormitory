565bdf007fd9759429ccf94ba0d32cf08aba12e0
Ju-MINJAE <juminjae98@gmail.com>
2024-07-09 14:46:52 +0900

    board, info page

---
+++ griffin/src/pages/info/InfoPage.vue
@@ -0,0 +1,17 @@
+<!-- InfoPage.vue -->
+<template>
+  <div>
+    <students-license></students-license>
+  </div>
+</template>
+
+<script>
+import StudentsLicense from '../../components/UI/StudentsLicense.vue';
+
+export default {
+  name: 'InfoPage',
+  components: {
+    StudentsLicense,
+  },
+};
+</script>
--- griffin/src/router.js
+++ griffin/src/router.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import InfoPage from './pages/info/InfoPage.vue';
+import BoardList from './pages/board/BoardList.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: null },
-    { path: '/p1', component: null },
-    { path: '/p2', component: null },
-    { path: '/p3', component: null },
-    { path: '/p4', component: null },
+    { path: '/', redirect: '/info' },
+    { path: '/info', component: InfoPage },
+    { path: '/boardlist', component: BoardList },
   ],
 });
 
