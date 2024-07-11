2589b65c17063bf3a9f8f19014a3e2b00e186c72
Ju-MINJAE <juminjae98@gmail.com>
2024-07-11 16:09:04 +0900

    BaseBtn component 설정

--- griffin/src/main.js
+++ griffin/src/main.js
@@ -4,11 +4,13 @@ import App from './App.vue';
 import router from './router';
 import store from './store/index';
 import BaseCard from './components/UI/BaseCard.vue';
+import BaseBtn from './components/UI/BaseBtn.vue';
 
 const app = createApp(App);
 app.use(router);
 app.use(store);
 
 app.component('BaseCard', BaseCard);
+app.component('BaseBtn', BaseBtn);
 
 app.mount('#app');
