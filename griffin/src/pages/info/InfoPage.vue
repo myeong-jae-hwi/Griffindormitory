496c08092d5b2f6bfc35a0488da4694edadc085a
Myeong Jae Hwi <audwognl@gmail.com>
2024-07-17 16:38:46 +0900

    Revert "Merge pull request #58 from myeong-jae-hwi/Jaehwi"
    This reverts commit 89ea6a2739acbaefaf30e9a971114361f903588e, reversing
    changes made to ac68dcd9ea751a89658630a18d80d4c1afdc8341.

--- griffin/src/pages/info/InfoPage.vue
+++ griffin/src/pages/info/InfoPage.vue
@@ -74,8 +74,6 @@ export default {
 <style scoped>
 h3 {
   margin-left: 20px;
-  margin-right: 20px;
-  border-bottom: dotted 1px;
 }
 .card {
   position: relative;
@@ -92,9 +90,9 @@ h3 {
 }
 
 li {
-  width: 100%;
-  border-top: solid 1px #ececec;
-  border-bottom: solid 1px #ececec;
+  width: 98%;
+  border-top: solid 0.5px lightslategray;
+  border-bottom: solid 0.5px lightslategray;
 }
 
 h3 {
