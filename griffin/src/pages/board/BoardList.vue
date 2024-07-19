9e207d858c0d75a5402fc7a759aff510b6fff809
Ju-MINJAE <juminjae98@gmail.com>
2024-07-19 15:13:28 +0900

    animation 추가

--- griffin/src/pages/board/BoardList.vue
+++ griffin/src/pages/board/BoardList.vue
@@ -19,7 +19,11 @@
         <base-btn v-if="isListRoute" class="board-btn">글 쓰기</base-btn>
       </router-link>
     </div>
-    <router-view></router-view>
+    <router-view v-slot="slotProps">
+      <transition name="board" mode="out-in">
+        <component :is="slotProps.Component"></component>
+      </transition>
+    </router-view>
   </section>
 </template>
 
@@ -52,7 +56,7 @@ export default {
   padding: 0 20px;
   list-style-type: disc;
 }
-.board-list{
+.board-list {
   display: block !important;
 }
 .btn-container {
@@ -61,5 +65,17 @@ export default {
 }
 h3 {
   text-align: center;
+}
+
+.board-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+.board-enter-active {
+  transition: all 0.3s ease-out;
+}
+.board-enter-to {
+  opacity: 1;
+  transform: translateY(0);
 }
 </style>
