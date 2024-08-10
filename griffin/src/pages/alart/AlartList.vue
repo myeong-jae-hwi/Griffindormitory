78e63c244c352ec504a697c855b7445a40dba0c6
Ju-MINJAE <juminjae98@gmail.com>
2024-08-10 23:06:14 +0900

    Update AlartList.vue

--- griffin/src/pages/alart/AlartList.vue
+++ griffin/src/pages/alart/AlartList.vue
@@ -1,85 +1,96 @@
 <template>
   <div>
+    <h2 v-if="allNotifications.length === 0" class="no-notifications">
+      알람이 없습니다.
+    </h2>
     <div v-if="loading">Loading...</div>
     <div v-if="error">{{ error.message }}</div>
     <ul v-if="!loading">
       <li v-for="notification in allNotifications" :key="notification.id">
         <base-card @click="GotoPost(notification)" class="list">
-          <p :class="{ isread: notification.is_read }">{{ notification.message }}</p>
+          <p :class="{ isread: notification.is_read }">
+            {{ notification.message }}
+          </p>
         </base-card>
       </li>
     </ul>
-
-    <!-- <button @click="createNewNotification">Create New Notification</button> -->
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
-import BaseCard from "../../components/UI/BaseCard.vue";
+import { mapGetters, mapActions } from 'vuex';
+import BaseCard from '../../components/UI/BaseCard.vue';
 
 export default {
   components: { BaseCard },
   computed: {
-    ...mapGetters("notifications", [
-      "allNotifications",
-      "notificationsLoading",
-      "notificationsError",
+    ...mapGetters('notifications', [
+      'allNotifications',
+      'notificationsLoading',
+      'notificationsError',
     ]),
-    ...mapGetters("users", ["currentUser"]),
+    ...mapGetters('users', ['currentUser']),
     userId() {
       return this.$store.state.users.userID;
     },
   },
   methods: {
-    ...mapActions("notifications", [
-      "fetchNotifications",
-      "markNotificationAsRead",
-      "createNotification",
+    ...mapActions('notifications', [
+      'fetchNotifications',
+      'markNotificationAsRead',
+      'createNotification',
     ]),
     markAsRead(notificationId) {
       const uid = this.userId;
       this.markNotificationAsRead({ uid, notificationId });
     },
-    GotoPost(notificationId){
-      this.markAsRead(notificationId.id)
+    GotoPost(notificationId) {
+      this.markAsRead(notificationId.id);
       this.$router.push({
-        path: `/boardlist/${notificationId.boardId}`,})
-    }
-
+        path: `/boardlist/${notificationId.boardId}`,
+      });
+    },
   },
   created() {
     const uid = this.userId;
-    console.log(this.notificationId)
+    console.log(this.notificationId);
     this.fetchNotifications({ uid });
   },
 };
 </script>
 
 <style scoped>
-li{
+.no-notifications {
+  color: #696969;
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-align: center;
+  margin-top: 20px;
+  padding: 10px;
+}
+li {
   list-style-type: none;
 }
 
-li:last-child{
+li:last-child {
   padding-bottom: 50px;
 }
 
-.isread{
+.isread {
   color: #696969;
   font-weight: normal;
 }
 
-p{
+p {
   font-weight: 700;
 }
 
-ul{
+ul {
   padding: 0;
 }
 
-.list{
-  padding-left:20px;
+.list {
+  padding-left: 20px;
   cursor: pointer;
 }
 </style>
