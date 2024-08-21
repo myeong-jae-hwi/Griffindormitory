400bc8b15a38e47bc183a6083bc2dc4fff1a9987
Ju-MINJAE <juminjae98@gmail.com>
2024-08-21 17:40:01 +0900

    board module

--- griffin/src/store/modules/board/mutations.js
+++ griffin/src/store/modules/board/mutations.js
@@ -8,6 +8,14 @@ export default {
   deleteBoard(state, boardId) {
     state.boards = state.boards.filter((board) => board.id !== boardId);
   },
+  UPDATE_BOARD(state, updatedBoard) {
+    const index = state.boards.findIndex(
+      (board) => board.id === updatedBoard.id
+    );
+    if (index !== -1) {
+      state.boards.splice(index, 1, updatedBoard);
+    }
+  },
   addComment(state, { boardId, comment }) {
     const board = state.boards.find((board) => board.id === boardId);
     if (board) {
